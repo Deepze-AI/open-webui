d66f1e048f9334e24524c7c16c228a6954da4278
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-03-20 14:56:44 -0300

    [DPZ] change WebUI and Open WebUI string to Deepze

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -259,7 +259,7 @@
 				if (done) {
 					if ($isLastActiveTab) {
 						if ($settings?.notificationEnabled ?? false) {
-							new Notification(`${title} | Open WebUI`, {
+							new Notification(`${title} | Deepze`, {
 								body: content,
 								icon: `${WEBUI_BASE_URL}/static/favicon.png`
 							});
