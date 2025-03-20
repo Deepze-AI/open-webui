5f5e0b04e400d9c53e814b271248e6d21e49e9a1
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-03-20 16:05:03 -0300

    [DPZ] change WebUI and Open WebUI string to Deepze

--- backend/open_webui/routers/openai.py
+++ backend/open_webui/routers/openai.py
@@ -260,7 +260,7 @@ async def speech(request: Request, user=Depends(get_verified_user)):
 
             raise HTTPException(
                 status_code=r.status_code if r else 500,
-                detail=detail if detail else "Open WebUI: Server Connection Error",
+                detail=detail if detail else "Deepze: Server Connection Error",
             )
 
     except ValueError:
@@ -502,7 +502,7 @@ async def get_models(
                 # ClientError covers all aiohttp requests issues
                 log.exception(f"Client error: {str(e)}")
                 raise HTTPException(
-                    status_code=500, detail="Open WebUI: Server Connection Error"
+                    status_code=500, detail="Deepze: Server Connection Error"
                 )
             except Exception as e:
                 log.exception(f"Unexpected error: {e}")
@@ -563,7 +563,7 @@ async def verify_connection(
             # ClientError covers all aiohttp requests issues
             log.exception(f"Client error: {str(e)}")
             raise HTTPException(
-                status_code=500, detail="Open WebUI: Server Connection Error"
+                status_code=500, detail="Deepze: Server Connection Error"
             )
         except Exception as e:
             log.exception(f"Unexpected error: {e}")
@@ -738,7 +738,7 @@ async def generate_chat_completion(
 
         raise HTTPException(
             status_code=r.status if r else 500,
-            detail=detail if detail else "Open WebUI: Server Connection Error",
+            detail=detail if detail else "Deepze: Server Connection Error",
         )
     finally:
         if not streaming and session:
@@ -815,7 +815,7 @@ async def proxy(path: str, request: Request, user=Depends(get_verified_user)):
                 detail = f"External: {e}"
         raise HTTPException(
             status_code=r.status if r else 500,
-            detail=detail if detail else "Open WebUI: Server Connection Error",
+            detail=detail if detail else "Deepze: Server Connection Error",
         )
     finally:
         if not streaming and session:
