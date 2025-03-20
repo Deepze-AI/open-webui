4ceefc1ed072355996e392444aff08164a6d7910
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-03-20 14:56:44 -0300

    [DPZ] change WebUI and Open WebUI string to Deepze

--- src/lib/components/admin/Settings/General.svelte
+++ src/lib/components/admin/Settings/General.svelte
@@ -582,7 +582,7 @@
 
 					<div class="mb-2.5 w-full justify-between">
 						<div class="flex w-full justify-between">
-							<div class=" self-center text-xs font-medium">{$i18n.t('Deepze UI URL')}</div>
+							<div class=" self-center text-xs font-medium">{$i18n.t('Deepze URL')}</div>
 						</div>
 
 						<div class="flex mt-2 space-x-2">
@@ -596,7 +596,7 @@
 
 						<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
 							{$i18n.t(
-								'Enter the public URL of your Deepze UI. This URL will be used to generate links in the notifications.'
+								'Enter the public URL of your Deepze. This URL will be used to generate links in the notifications.'
 							)}
 						</div>
 					</div>
